a3574c44d7a1247f2b5d6c7a4dbb5d6f410ef3d2
burn-sie <96800768+burn-sie@users.noreply.github.com>
2024-08-31 14:05:06 -0400

    Update REMOVE BLOAT.bat
    Added a few goto statements in case someone enters wrong input.
    
    Added a few more packages to remove.
    
    These new lines do break Factory Reset button from settings. You will need to run "fastboot -w" to factory reset. Also break LenovoID login, which is fine because bloat.
    
    After reset, the system now only uses 1.7gb of ram on a fresh install.

--- REMOVE BLOAT.bat
+++ REMOVE BLOAT.bat
@@ -1,9 +1,12 @@
 @echo off
 cls
 @echo Do you want to remove bloat from "Lenovo Smart Tab with Google" aka "YT-X705F" ?
+@echo This script will break LenovoID and the factory reset button in Settings.
 set /p prompt=Are You Sure?[Y/N]: 
 if %prompt%== Y goto CONTINUE
+if %prompt%== y goto CONTINUE
 if not %prompt%== Y exit
+exit
 
 :CONTINUE
 @echo You must grant adb SuperUser permissions on your tablet in order for this script to work.
@@ -11,29 +14,21 @@ TIMEOUT /T 5
 echo.
 cls
 
+:SENDSU
 @echo Sending Super User request. Please Press Grant on the tablet then 
-
-:: The following line unmounts the system partition and remounts it as writable.
 adb shell su -c mount --remount / -w
-
 echo.
-
 cls
-::Another Prompt
 set /P surequest=Did you press the grant button on the tablet?[Y/N]:
 if %surequest%== Y goto DELETE
-if not %surequest%== Y exit
-
-
+if %surequest%== y goto DELETE
+if not %surequest%== Y goto SENDSU
+exit
 
 :DELETE
 @echo Deleting.... DONT CLOSE THE WINDOW!!
 @echo off
-:: The following line unmounts the system and vendor partition and remounts it as writable.
 adb shell su -c mount --remount / -w
-adb shell su -c mount --remount /vendor -w
-
-::The Following starts deleting things aka bloat. Breaks multi user though.
 adb shell su -c rm -frR /system/app/Outlook/
 adb shell su -c rm -frR /system/app/YandexSearch/
 adb shell su -c rm -frR /system/app/YandexBrowser/
@@ -61,22 +56,26 @@ adb shell su -c rm -frR /system/product/priv-app/Contacts/
 adb shell su -c rm -frR /system/priv-app/LenovoWhatsNewClient/
 adb shell su -c rm -frR /system/priv-app/Podcasts/
 adb shell su -c rm -frR /system/priv-app/KidCtrl/
-adb shell su -c rm -frR /system/app/FT_Terminal_Test/
-adb shell su -c rm -frR /system/priv-app/MultiUserSetting_ROW/
-adb shell su -c rm -frR /vendor/app/LenovoRUStub/
-adb shell su -c rm -frR /vendor/operator/app/AudioBook/
-adb shell su -c rm -frR /system/app/LenovoUserExperience_ROW/
-adb shell su -c rm -frR /system/priv-app/Theme_3.3.20LE/
-adb shell su -c rm -frR /system/priv-app/LSF_DeviceService/
-adb shell su -c rm -frR /system/priv-app/OCZ_ClientDownloader/
-adb shell su -c rm -frR /system/priv-app/OCZ_DeployServiceApp/
-adb shell su -c rm -frR /system/priv-app/StatementService/
 
-
+adb shell su -c rm -frR /system/priv-app/LenovoOTA/LenovoOTA.apk
+adb shell su -c rm -frR /system/priv-app/LSF_LenovoID/LenovoID-Pad-RoW.apk
+adb shell su -c rm -frR /system/priv-app/LSF_DeviceService/lsf_device_V5.0.2.0058trn.apk
+adb shell su -c rm -frR /system/priv-app/OCZ_ClientDownloader/OCZ_ClientDownloader.apk
+adb shell su -c rm -frR /system/priv-app/OCZ_DeployServiceApp/OCZ_DeployServiceApp.apk
+adb shell su -c rm -frR /system/priv-app/SetupWizardExt_ROW/SetupWizardExt_ROW.apk
+adb shell su -c rm -frR /system/priv-app/LenovoWallpaper/LenovoWallpaper.apk
+adb shell su -c rm -frR /system/app/Traceur/Traceur.apk
+adb shell su -c rm -frR /system/app/Midtest/Midtest.apk
+adb shell su -c rm -frR /system/app/ExactCalculator/ExactCalculator.apk
+adb shell su -c rm -frR /vendor/app/LenovoRUStub/LenovoRUStub.apk
+adb shell su -c rm -frR /vendor/app/SSGTelemetryService/SSGTelemetryService.apk
+adb shell su -c rm -frR /vendor/operator/app/AudioBook/audiobook_v1464_2020-07-29_release.apk
+adb shell su -c pm uninstall --user 0 com.lct.lottie
 
 cls
 adb reboot
 @echo Tablet restarted, if you ran this script on a fresh reset, the bloat should be gone. 
 @echo If you didnt reset, you can now uninstall bloat apps normally.
+@echo To Factory Reset your device, run "fastboot -w" from you computer while in fastboot.
 TIMEOUT /T 10
 exit
